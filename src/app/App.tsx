68c699b59dea6422fffd7034003ae92a26b7c1fc
Arshad Khan <arshdkhn1@gmail.com>
2020-04-26 16:14:30 +0200

    Init 🔥

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Projects from "./views/Projects";
+
+export default function App() {
+  return (
+    <>
+      <header className='bg-gray-900 text-white flex items-center h-12 w-full'>
+        <div className='container mx-auto'>
+          <a className='navbar-brand' href='/'>
+            Timelogger
+          </a>
+        </div>
+      </header>
+
+      <main>
+        <div className='container mx-auto'>
+          <Projects />
+        </div>
+      </main>
+    </>
+  );
+}
